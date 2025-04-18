bac9ee153f8443000be44c26555deafbcee1230a
Bishara1 <bish.150b@gmail.com>
2025-04-18 22:42:22 +0300

    Update components.py

--- utils/components.py
+++ utils/components.py
@@ -6,24 +6,34 @@ GRAY = (100, 100, 100)
 GREEN = (0, 255, 0)
 
 class Cell(pygame.sprite.Sprite):
-    def __init__(self, x=None, y=None, w=None, h=None, color=None, value=None):
+    def __init__(self, x=None, y=None, w=None, h=None, color=None, value=None, is_circle=None, radius=None):
         super().__init__()
         self.x = x
         self.y = y
+        self.w = w
+        self.h = h
         self.rect = pygame.Rect(x, y, w, h)
         self.surf = pygame.Surface((w, h))
         self.surf.fill((255, 255,255))
         self.value = value
         self.originalColor = color
         self.color = color
+        self.is_circle = is_circle 
+        self.radius = radius
     
     def draw(self, surface):
-        pygame.draw.rect(surface, self.color, self.rect, 20 if self.color != GREEN else 0)
+        if self.is_circle:
+            pygame.draw.rect(surface, self.color, self.rect, 20 if self.color != GREEN else 0)
+        else:
+            pygame.draw.circle(surface, self.color, (self.w, self.h), self.radius, 15)
     
     def updateColor(self, surface, color):
         self.color = color
-        pygame.draw.rect(surface, self.color, self.rect, 20 if self.color != GREEN else 0)
+        self.draw()
     
     def resetColor(self, surface):
         self.color = self.originalColor
-        pygame.draw.rect(surface, self.color, self.rect, 20 if self.color != GREEN else 0)
+        self.draw()
+    
+
+
